cf7e0638e4a533dd1c634c09b07228a32553e3eb
ariq <user@localhost>
2022-11-30 07:16:22 +0700

    Add logic 1. Run camera only on bluetooth user verified 2. When board library is failed to import assume running outside pi

--- RelayControl.py
+++ RelayControl.py
@@ -1,20 +1,31 @@
-from pydbus.generic import signal
+from pydbus.generic import signal as dbus_signal
 from pydbus import SessionBus, SystemBus
 from gi.repository import GLib
 import RPi.GPIO as Pin
 import time
-import board
-import neopixel
 import math
+try:
+    import board
+    import neopixel
+    RelayPin = board.D12
+    LEDPIN = board.D18
+    bus = SystemBus()
+    runOnPI = True
+except ModuleNotFoundError:
+    #Assuming not running on pi
+    RelayPin = 22
+    LEDPIN = 18
+    bus = SessionBus()
+    runOnPI = False
+
 
 busName = "com.sisalma.pydbus"
 RELAYON = 0
 RELAYOFF = 1
-RelayPin = 22
+
 LEDGREEN = (0,255,12)
 LEDRED = (255,12,0)
 LEDOFF = (0,0,0)
-LEDPIN = board.D18
 
 class RelayLogic:
     """
@@ -35,21 +46,28 @@ class RelayLogic:
     </node>
     """
     def __init__(self):
-        self.pixels = neopixel.NeoPixel(board.D18, 1)
+        self.useLED = runOnPI
+        if self.useLED:
+            self.pixels = neopixel.NeoPixel(board.D18, 1)
         self.RelayState = RELAYOFF
         self._helmetDetected = False
         self._bluetoothKeyVerified = False
         Pin.setmode(Pin.BCM)
         Pin.setup(RelayPin, Pin.OUT)
         self.stateAction()
+        
 
     def BluetoothKeyStatus(self, s):
-        self._bluetoothKeyVerified = s
-        self.lockUnlockIO()
+        #Only trigger on State Changes
+        if s != self._bluetoothKeyVerified:
+            self._bluetoothKeyVerified = s
+            self.lockUnlockIO()
         
     def HelmetStatus(self, s):
-        self._helmetDetected = s
-        self.lockUnlockIO()
+        #Only trigger on State Changes
+        if s != self._helmetDetected:
+            self._helmetDetected = s
+            self.lockUnlockIO()
     
     def lockUnlockIO(self):
         if self._bluetoothKeyVerified and self._helmetDetected:
@@ -57,36 +75,39 @@ class RelayLogic:
         elif self._bluetoothKeyVerified and not self._helmetDetected:
             self.RelayState = RELAYOFF
         elif not self._bluetoothKeyVerified :
+            self.RelayState = RELAYOFF
             #Send signal to stop detecting helmet on other script listening
-            pass
+            self.PropertiesChanged(busName, {"bluetoothKeyVerified": self._bluetoothKeyVerified}, [])
         self.stateAction()
         
     def stateAction(self):
         if self.RelayState == RELAYON:
-            self.pixels[0] = LEDGREEN
+            self.changeLEDColor(LEDGREEN)
             Pin.output(RelayPin,Pin.HIGH)
+            print("Relay state changes to: ON")
         elif self.RelayState == RELAYOFF:
-            self.pixels[0] = LEDRED
+            self.changeLEDColor(LEDRED)
             Pin.output(RelayPin,Pin.LOW)
+            print("Relay state changes to: OFF")
+        
         
     @property
-    def _BluetoothKeyVerified(self):
+    def bluetoothKeyVerified(self):
         return self._bluetoothKeyVerified
 
-    @_BluetoothKeyVerified.setter
-    def LockStatus(self, value):
-        self._someProperty = value
-        self.PropertiesChanged(busName, {"LockStatus": self.LockStatus}, [])
+    def changeLEDColor(self,color):
+        if self.useLED:
+            self.pixels[0] = color
+        else:
+            print("Program not running on Pi, disabling neopixel library")
+    
+    def stopObject(self):
+        self.changeLEDColor(LEDOFF) 
 
-    def Stop(self):
-        self.pixels[0] = LEDOFF
-
-    PropertiesChanged = signal()
+    PropertiesChanged = dbus_signal()
 
 def main():
     loop = GLib.MainLoop()
-    #bus = SessionBus()
-    bus = SystemBus()
     RelayObject = RelayLogic()
     bus.publish(busName,RelayObject)
     loop.run()
---
+++ helmetAnnouncer.py
@@ -0,0 +1,30 @@
+from pydbus import SessionBus, SystemBus
+from camera import detector
+from cv2 import VideoCapture as selectCamera
+from gi.repository import GLib
+import time
+try:
+    import board
+    import neopixel
+    bus = SystemBus()
+except ModuleNotFoundError:
+    bus = SessionBus()
+
+
+def main():
+    td = detector.ImageProcessor(selectCamera(0))
+    fd = detector.FaceDetector(td,128,128,1.7)
+    hd = detector.HelmetDetector(fd,0.7)
+    Controller = bus.get("com.sisalma.pydbus")
+    try:
+        while(True):
+            if Controller.bluetoothKeyVerified:
+                result = hd.helmetUsed(12)
+                print(result)
+                Controller.HelmetStatus(result)
+            else:
+                print("Won't run: BluetoothKey is not verified")
+                time.sleep(1)
+    except GLib.error as err:
+        print("Complementary program exitting")
+main()
