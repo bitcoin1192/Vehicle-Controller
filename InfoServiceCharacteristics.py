6fced1228a9a0a3832a4ce00bf28ab8b6848169b
ariq <user@localhost>
2022-12-02 08:50:50 +0700

    few experiment

--- InfoServiceCharacteristics.py
+++ InfoServiceCharacteristics.py
@@ -4,10 +4,21 @@ from uuidConstant import *
 
 
 class lockStatus(gattCharacteristics):
-    current_status = UNLOCKED
+    def __init__(self, bus, index, uuid, flags, service):
+        self.current_status = UNLOCKED
+        self.buf = ""
+        super().__init__(bus, index, uuid, flags, service)
+
     def statusUpdate(self,lockUpdate):
         self.current_status = lockUpdate
 
+    def WriteValue(self, value, options):
+        if value == '\0':
+            print()
+            self.buf == ""
+        else:
+            self.buf += value
+        
     def ReadValue(self, options):
         return [dbus.Boolean(self.current_status)]
 
