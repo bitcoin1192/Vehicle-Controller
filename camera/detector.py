13fc7c05936b8ba39020568fba0cece9edb6efe5
Ariq <dhiaariq@live.com>
2023-04-03 10:29:38 +0700

    use debugFlagDetector global variable to control debug output When debugging it print how fast it processes images, then write whole image to a file and another file contain image crop with inference result in it's file name

--- camera/detector.py
+++ camera/detector.py
@@ -16,8 +16,8 @@ tflitePath = "/home/pi/final-skripsi/camera/model-resnet50-relu-1.tflite"
 matrixCalibPath = "/home/pi/final-skripsi/camera/mtx.correction-2.npy"
 distortionCalibPath = "/home/pi/final-skripsi/camera/dist.correction-2.npy"
 
-debug = False
-
+#Global Variable
+debugFlagDetector = False
 frameTotal = 1
 
 class ImageProcessor:
@@ -30,7 +30,6 @@ class ImageProcessor:
         self.mtxconst = np.load(matrixCalibPath)
         self.distconst = np.load(distortionCalibPath)
         self.newcameramtx, self.roi = cv2.getOptimalNewCameraMatrix(self.mtxconst, self.distconst, (1280,720), 1, (1280,720))
-        self.debugFlag = False
         self.toc = 0
         self.tic = 0
 
@@ -54,10 +53,13 @@ class ImageProcessor:
         
     def ImagePreProcessing(self):
         #Convert camera input to opencv gray output
+        global debugFlagDetector
         inputImage = self.__getImage()
         norm = np.zeros((128,128))
         gray = cv2.cvtColor(inputImage, cv2.COLOR_RGB2GRAY)
         norm = cv2.normalize(gray,norm,0,255,cv2.NORM_MINMAX)
+        if debugFlagDetector:
+            self.debug()
         return (gray,inputImage)
         #Normalize gray frame
         #norm = cv2.normalize(gray,norm,0,255,cv2.NORM_MINMAX)
@@ -65,10 +67,9 @@ class ImageProcessor:
         #image_norm = cv2.equalizeHist(gray)
     
     def debug(self):
-        if self.debugFlag is True:
-            self.toc = self.tic
-            self.tic = time.time()
-            print("I'm fetching image at {} fps".format(1000/((self.tic-self.toc)*1000)))
+        self.toc = self.tic
+        self.tic = time.time()
+        print("I'm fetching image at {} fps".format(1000/((self.tic-self.toc)*1000)))
     
 
 class FaceDetector:
@@ -80,10 +81,9 @@ class FaceDetector:
         self.resizeToPixel = outputSize
         self.face_cascade = cv2.CascadeClassifier(cvHaarPath)
         self.tempImage = 0
-        self.debugFlag = debug
-        #self.frameTotal = 1
     
     def getFace(self):
+        global debugFlagDetector
         imageTupple = self.ImgProcess.ImagePreProcessing()
         grayImage = imageTupple[0]
         colorImage = imageTupple[1]
@@ -102,7 +102,8 @@ class FaceDetector:
                 else:
                     print("Detecting face")
                     resizeImage = cv2.resize(colorImage[start[1]:end[1],start[0]:end[0]],(128,128),cv2.INTER_AREA)
-                    #self.debug(resizeImage)
+                    if debugFlagDetector:
+                        self.debug(resizeImage)
                     faceImage.append(resizeImage)
         self.tempImage = faceImage
         return faceImage
@@ -135,7 +136,6 @@ class HelmetDetector:
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
-        #self.frameTotal = 1
     
     def debug(self, resizeImage, detectResult):
         global frameTotal
@@ -178,19 +178,22 @@ class HelmetDetector:
         return(None,None)
 
     def detectHelmet(self):
+        global debugFlagDetector
         output_data,face = self.runPrediction()
         if output_data is not None:
             if output_data[0][0] > output_data[0][1] and output_data[0][0] > self.confidence:
                 plus = np.array([1,0])
                 self.tallyCounter = np.add(self.tallyCounter,plus)
                 txtPrintImg = ["Helmet Detected",(0,255,0)]
-                self.debug(face,"helmet-{}".format(output_data[0][0]))
+                if debugFlagDetector:
+                    self.debug(face,"helmet-{}".format(output_data[0][0]))
                 print(txtPrintImg[0]) 
             if output_data[0][1] > output_data[0][0] and output_data[0][1] > self.confidence:
                 plus = np.array([0,1])
                 self.tallyCounter = np.add(self.tallyCounter,plus)
                 txtPrintImg = ["Helmet not Detected",(0,0,255)]
-                self.debug(face,"nohelmet-{}".format(output_data[0][1]))
+                if debugFlagDetector:
+                    self.debug(face,"nohelmet-{}".format(output_data[0][1]))
                 print(txtPrintImg[0])
             self.counter += 1
 
