29e11d59593609fd044a7b3ddb24f083f4208d0e
bitcoin1192 <dhiaariq@live.com>
2023-06-10 02:49:14 +0100

    disable debug and change characteristic

--- camera/detector.py
+++ camera/detector.py
@@ -17,7 +17,7 @@ matrixCalibPath = "/home/pi/final-skripsi/camera/mtx.correction-2.npy"
 distortionCalibPath = "/home/pi/final-skripsi/camera/dist.correction-2.npy"
 
 #Global Variable
-debugFlagDetector = True
+debugFlagDetector = False
 frameTotal = 1
 
 class ImageProcessor:
